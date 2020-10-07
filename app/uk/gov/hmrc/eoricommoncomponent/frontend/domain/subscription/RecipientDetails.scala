f29a0bb14e75d13a92c344a3c4e2d93f873e7049
Steve Sugden <46934286+SteveSugden@users.noreply.github.com>
2020-10-07 10:35:03 +0100

    ECC-12 - use enrolment key for RecipientDetails.service (#59)

--- app/uk/gov/hmrc/eoricommoncomponent/frontend/domain/subscription/RecipientDetails.scala
+++ app/uk/gov/hmrc/eoricommoncomponent/frontend/domain/subscription/RecipientDetails.scala
@@ -23,7 +23,7 @@ import uk.gov.hmrc.eoricommoncomponent.frontend.views.ServiceName
 
 case class RecipientDetails(
   journey: Journey.Value,
-  service: String,
+  enrolmentKey: String,
   serviceName: String,
   recipientEmailAddress: String,
   recipientFullName: String,
@@ -45,7 +45,7 @@ object RecipientDetails {
   )(implicit messages: Messages): RecipientDetails =
     RecipientDetails(
       journey,
-      service.code,
+      service.enrolmentKey,
       ServiceName.longName(service),
       recipientEmailAddress,
       recipientFullName,
@@ -59,7 +59,7 @@ object RecipientDetails {
   ): RecipientDetails =
     RecipientDetails(
       journey,
-      service.code,
+      service.enrolmentKey,
       ServiceName.longName(service),
       contactDetails.emailAddress,
       contactDetails.fullName,
