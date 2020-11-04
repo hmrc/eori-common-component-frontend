298a55c13a1adc474845d2adfd994649a92e80d7
Steve Sugden <46934286+SteveSugden@users.noreply.github.com>
2020-11-04 08:45:07 +0000

    ECC-255 - error message problems around input field

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,1 +1,9 @@
 document.body.className = 'js-enabled';
+
+$(document).ready(function () {
+    // Use GOV.UK shim-links-with-button-role.js to trigger a link styled to look like a button,
+    // with role="button" when the space key is pressed.
+    GOVUK.shimLinksWithButtonRole.init()
+
+    document.getElementById("errors").focus();
+})
