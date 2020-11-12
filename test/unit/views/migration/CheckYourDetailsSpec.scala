df952466a18adef7979edc30db3c77af792c089c
Steve Sugden <46934286+SteveSugden@users.noreply.github.com>
2020-11-12 14:22:35 +0000

    ECC-274 - display change links for RoW UTR and NINO

--- test/unit/views/migration/CheckYourDetailsSpec.scala
+++ test/unit/views/migration/CheckYourDetailsSpec.scala
@@ -277,7 +277,7 @@ class CheckYourDetailsSpec extends ViewSpec {
       page.body.getElementById("review-tbl__name-and-address_heading").text mustBe "Organisation address"
     }
 
-    "display change link for the following for utr if isThirdCountrySubscription " in {
+    "display change link for the following for utr if isThirdCountrySubscription organisation" in {
       val page = doc(
         orgType = Some(CdsOrganisationType.ThirdCountryOrganisation),
         isThirdCountrySubscription = true,
@@ -288,10 +288,36 @@ class CheckYourDetailsSpec extends ViewSpec {
       ) mustBe "/customs-enrolment-services/atar/subscribe/row-utr"
     }
 
-    "display change link for the following for nino if isThirdCountrySubscription " in {
+    "display change link for the following for utr if isThirdCountrySubscription individual" in {
+      val page = doc(
+        isIndividualSubscriptionFlow = true,
+        customsId = utr,
+        orgType = Some(CdsOrganisationType.ThirdCountryIndividual),
+        isThirdCountrySubscription = true,
+        nameIdOrganisationDetails = None
+      )
+      page.body.getElementById("review-tbl__utr_change").attr(
+        "href"
+      ) mustBe "/customs-enrolment-services/atar/subscribe/row-utr"
+    }
+
+    "display change link for the following for nino if isThirdCountrySubscription organisation" in {
       val page = doc(
         customsId = nino,
         orgType = Some(CdsOrganisationType.ThirdCountryOrganisation),
+        isThirdCountrySubscription = true,
+        nameIdOrganisationDetails = None
+      )
+      page.body.getElementById("review-tbl__nino_change").attr(
+        "href"
+      ) mustBe "/customs-enrolment-services/atar/subscribe/row-nino"
+    }
+
+    "display change link for the following for nino if isThirdCountrySubscription individual" in {
+      val page = doc(
+        isIndividualSubscriptionFlow = true,
+        customsId = nino,
+        orgType = Some(CdsOrganisationType.ThirdCountryIndividual),
         isThirdCountrySubscription = true,
         nameIdOrganisationDetails = None
       )
