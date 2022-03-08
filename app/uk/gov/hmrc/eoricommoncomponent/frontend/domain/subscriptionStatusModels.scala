cfd727242a4ba9b281785ea60f140343d1995994
NandanaRaghu <60367890+NandanaRaghu@users.noreply.github.com>
2022-03-08 16:55:09 +0000

    ECC-585: scalafmt issue fixed

--- app/uk/gov/hmrc/eoricommoncomponent/frontend/domain/subscriptionStatusModels.scala
+++ app/uk/gov/hmrc/eoricommoncomponent/frontend/domain/subscriptionStatusModels.scala
@@ -22,12 +22,7 @@ import uk.gov.hmrc.eoricommoncomponent.frontend.domain.messaging.CommonHeader
 
 import java.time.format.DateTimeFormatter
 
-case class SubscriptionStatusQueryParams(
-  receiptDate: LocalDateTime,
-  regime: String,
-  idType: String,
-  id: String
-) {
+case class SubscriptionStatusQueryParams(receiptDate: LocalDateTime, regime: String, idType: String, id: String) {
 
   def queryParams: Seq[(String, String)] = {
     val receiptDateAsString =
