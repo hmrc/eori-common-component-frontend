a0a0aa056a624168cc3beeaf1e891e47e72a7cd2
Jake Goodwin <12535866+JakeGoodwin@users.noreply.github.com>
2023-03-17 10:25:38 +0000

    ECC-1645: Upgrade Scala to 2.13.8

--- test/integration/IntegrationTestsSpec.scala
+++ test/integration/IntegrationTestsSpec.scala
@@ -25,6 +25,8 @@ import util.WireMockRunner
 import scala.concurrent.duration._
 import scala.concurrent.{Await, Future}
 
+import scala.language.postfixOps
+
 class IntegrationTestsSpec
     extends PlaySpec with BeforeAndAfter with BeforeAndAfterAll with Eventually with IntegrationPatience
     with GuiceOneAppPerSuite with WireMockRunner {
