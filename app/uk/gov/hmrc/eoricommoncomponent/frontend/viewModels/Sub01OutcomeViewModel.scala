2d4a6620d81a3e6787332595ebf220f8f02c7d31
German_ <81871966+dinamit1984@users.noreply.github.com>
2023-07-05 11:34:36 +0100

    ECC-1867 code refactoring

---
+++ app/uk/gov/hmrc/eoricommoncomponent/frontend/viewModels/Sub01OutcomeViewModel.scala
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.eoricommoncomponent.frontend.viewModels
+
+import play.api.i18n.Messages
+import play.api.mvc.Request
+import uk.gov.hmrc.eoricommoncomponent.frontend.views.ServiceName.{longName, shortName}
+
+object Sub01OutcomeViewModel {
+
+  def headingForRejected(name: Option[String])(implicit messages: Messages, request: Request[_]) = name match {
+    case Some(user) => messages(s"cds.sub01.outcome.rejected.subscribe.heading", longName, user)
+    case None       => messages(s"cds.sub01.outcome.rejected.subscribe.heading-noname", longName)
+  }
+
+  def headingForProcessing(name: Option[String])(implicit messages: Messages, request: Request[_]) = name match {
+    case Some(org) => messages("cds.sub01.outcome.processing.heading", shortName, org)
+    case None      => messages("cds.sub01.outcome.processing.heading-noname", shortName)
+
+  }
+
+}
---
+++ test/unit/viewModels/YouCantUseServiceViewModelSpec.scala
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package unit.viewModels
+
+import base.UnitSpec
+import play.api.test.FakeRequest
+import uk.gov.hmrc.auth.core.AffinityGroup
+import uk.gov.hmrc.eoricommoncomponent.frontend.viewModels.YouCantUseServiceViewModel
+import uk.gov.hmrc.eoricommoncomponent.frontend.views.ServiceName.longName
+import util.ControllerSpec
+
+class YouCantUseServiceViewModelSpec extends UnitSpec with ControllerSpec {
+  implicit val fakeRequest = FakeRequest()
+
+  "YouCantUseServiceViewModel firstParagraph" should {
+    "return the correct first paragraph for the 'Agent' user type" in {
+      val name   = Option(AffinityGroup.Agent)
+      val result = YouCantUseServiceViewModel.firstParagraph(name)(messages, fakeRequest)
+      result shouldBe messages("cds.you-cant-use-service-agent.para1")
+    }
+    "return the correct first paragraph for other user types" in {
+
+      val result = YouCantUseServiceViewModel.firstParagraph(None)(messages, fakeRequest)
+      result shouldBe messages("cds.you-cant-use-service-standard-organisation.para1", longName)
+    }
+
+  }
+  "YouCantUseServiceViewModel.secondParagraph" should {
+    "return the correct second paragraph for the 'Agent' user type" in {
+      val name   = Option(AffinityGroup.Agent)
+      val result = YouCantUseServiceViewModel.secondParagraph(name)(messages, fakeRequest)
+      result shouldBe messages("cds.you-cant-use-service-agent.para2", longName)
+    }
+    "return the correct second paragraph for other user types" in {
+
+      val result = YouCantUseServiceViewModel.secondParagraph(None)(messages, fakeRequest)
+      result shouldBe messages("cds.you-cant-use-service-standard-organisation.para2")
+    }
+
+  }
+
+}
