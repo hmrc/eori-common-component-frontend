ddf2d8da5f39d20a4689ceed0c602b56de2b35b4
Ming Sun <ming.sun@digital.hmrc.gov.uk>
2025-05-30 14:56:14 +0100

    DDCYLS-6783: upgrade libs

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,13 +8,13 @@ object AppDependencies {
 
   val compileDependencies: Seq[ModuleID] = Seq(
     "org.typelevel"     %% "cats-core"                             % "2.10.0",
-    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "2.0.0",
+    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.3.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"            % bootstrapVersion,
-    "uk.gov.hmrc"       %% "domain-play-30"                        % "9.0.0",
+    "uk.gov.hmrc"       %% "domain-play-30"                        % "10.0.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                    % mongoDbVersion,
     "uk.gov.hmrc"       %% "emailaddress-play-30"                  % "4.1.0",
     "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "9.11.0",
-    "uk.gov.hmrc"       %% "internal-auth-client-play-30"          % "2.0.0"
+    "uk.gov.hmrc"       %% "internal-auth-client-play-30"          % "4.0.0"
   )
 
   val testDependencies: Seq[ModuleID] = Seq(
