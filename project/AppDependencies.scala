54700368c717c650878a01ba5ddcb1ff5dc1e626
Ming Sun <86245839+sunmingdh@users.noreply.github.com>
2025-06-02 16:37:05 +0100

    DDCYLS-6783: remove uk.gov.hmrc: emailaddress (#625)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -10,7 +10,7 @@ object AppDependencies {
     "org.typelevel"     %% "cats-core"                             % "2.10.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.3.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"            % bootstrapVersion,
-    "uk.gov.hmrc"       %% "domain-play-30"                        % "10.0.0",
+    "uk.gov.hmrc"       %% "domain-play-30"                        % "11.0.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                    % mongoDbVersion,
     "uk.gov.hmrc"       %% "emailaddress-play-30"                  % "4.1.0",
     "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "9.11.0",
