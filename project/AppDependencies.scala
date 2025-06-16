7cec785a4e22923968c059e89f34afe33aaf4018
Ming Sun <86245839+sunmingdh@users.noreply.github.com>
2025-06-16 17:30:05 +0100

    DDCYLS-6783: remove emailaddress library (#627)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -11,7 +11,6 @@ object AppDependencies {
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.3.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"            % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                    % mongoDbVersion,
-    "uk.gov.hmrc"       %% "emailaddress-play-30"                  % "4.1.0",
     "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "9.11.0",
     "uk.gov.hmrc"       %% "internal-auth-client-play-30"          % "4.0.0"
   )
