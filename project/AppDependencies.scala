8686821056dc1588940b5ea853639a3f7b8c839c
AbdulHQuadri <67271481+AbdulHQuadri@users.noreply.github.com>
2026-02-16 14:01:12 +0000

    Dependency vulnerability update (#639)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,10 +9,13 @@ object AppDependencies {
   val compileDependencies: Seq[ModuleID] = Seq(
     "org.typelevel"     %% "cats-core"                             % "2.13.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.4.0",
-    "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"            % bootstrapVersion,
+    "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"            % bootstrapVersion exclude("org.apache.commons", "commons-lang3"),
+    "org.apache.commons" % "commons-lang3"                         % "3.18.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                    % mongoDbVersion,
     "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "12.25.0",
-    "uk.gov.hmrc"       %% "internal-auth-client-play-30"          % "4.3.0"
+    "uk.gov.hmrc"       %% "internal-auth-client-play-30"          % "4.3.0",
+    "ch.qos.logback"     % "logback-core"                          % "1.5.21",
+    "at.yawk.lz4"        %  "lz4-java"                             % "1.10.3",
   )
 
   val testDependencies: Seq[ModuleID] = Seq(
