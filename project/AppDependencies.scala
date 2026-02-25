d6a8804eb697e66fc56a7d7dd21dd133fd976cc3
accenture-dami <203887119+accenture-dami@users.noreply.github.com>
2026-02-25 10:01:03 +0000

    Platform upgrades

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,18 +3,18 @@ import sbt._
 object AppDependencies {
   import play.core.PlayVersion
 
-  val mongoDbVersion   = "2.11.0"
+  val mongoDbVersion   = "2.12.0"
   val bootstrapVersion = "10.5.0"
 
   val compileDependencies: Seq[ModuleID] = Seq(
     "org.typelevel"     %% "cats-core"                             % "2.13.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.4.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"            % bootstrapVersion exclude("org.apache.commons", "commons-lang3"),
-    "org.apache.commons" % "commons-lang3"                         % "3.18.0",
+    "org.apache.commons" % "commons-lang3"                         % "3.20.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                    % mongoDbVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "12.25.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "12.31.0",
     "uk.gov.hmrc"       %% "internal-auth-client-play-30"          % "4.3.0",
-    "ch.qos.logback"     % "logback-core"                          % "1.5.21",
+    "ch.qos.logback"     % "logback-core"                          % "1.5.32",
     "at.yawk.lz4"        %  "lz4-java"                             % "1.10.3",
   )
 
@@ -25,7 +25,7 @@ object AppDependencies {
     "org.scalacheck"         %% "scalacheck"              % "1.19.0"            % "test,it",
     "uk.gov.hmrc"            %% "bootstrap-test-play-30"  % bootstrapVersion    % "test,it",
     "org.scalatestplus"      %% "scalacheck-1-15"         % "3.2.11.0"          % "test,it",
-    "org.jsoup"               % "jsoup"                   % "1.21.2"            % "test,it",
+    "org.jsoup"               % "jsoup"                   % "1.22.1"            % "test,it",
     "us.codecraft"            % "xsoup"                   % "0.3.7"             % "test,it",
     "org.mockito"             % "mockito-core"            % "5.21.0"            % "test,it",
     "org.scalatestplus"      %% "mockito-4-6"             % "3.2.15.0"          % "test, it",
