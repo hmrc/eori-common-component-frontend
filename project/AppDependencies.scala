19edb422d5c6da9bef664a6c226e56d92df98ef6
AbdulHQuadri <67271481+AbdulHQuadri@users.noreply.github.com>
2026-02-25 16:10:46 +0000

    Platform upgrades

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,8 +3,8 @@ import sbt._
 object AppDependencies {
   import play.core.PlayVersion
 
-  val mongoDbVersion   = "2.11.0"
-  val bootstrapVersion = "10.5.0"
+  val mongoDbVersion   = "2.12.0"
+  val bootstrapVersion = "10.6.0"
 
   val compileDependencies: Seq[ModuleID] = Seq(
     "org.typelevel"     %% "cats-core"                             % "2.13.0",
@@ -12,7 +12,7 @@ object AppDependencies {
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"            % bootstrapVersion exclude("org.apache.commons", "commons-lang3"),
     "org.apache.commons" % "commons-lang3"                         % "3.18.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                    % mongoDbVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "12.25.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "12.31.0",
     "uk.gov.hmrc"       %% "internal-auth-client-play-30"          % "4.3.0",
     "ch.qos.logback"     % "logback-core"                          % "1.5.21",
     "at.yawk.lz4"        %  "lz4-java"                             % "1.10.3",
