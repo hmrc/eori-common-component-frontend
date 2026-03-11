345861ef778bb1b23caf801e174d1dda18b9a1c9
AbdulHQuadri <67271481+AbdulHQuadri@users.noreply.github.com>
2026-03-11 11:33:05 +0000

    Platbot comments

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,14 +4,14 @@ object AppDependencies {
   import play.core.PlayVersion
 
   val mongoDbVersion   = "2.12.0"
-  val bootstrapVersion = "10.6.0"
+  val bootstrapVersion = "10.7.0"
 
   val compileDependencies: Seq[ModuleID] = Seq(
     "org.typelevel"     %% "cats-core"                             % "2.13.0",
-    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.4.0",
+    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.5.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"            % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                    % mongoDbVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "12.31.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "12.32.0",
     "uk.gov.hmrc"       %% "internal-auth-client-play-30"          % "4.3.0"
   )
 
