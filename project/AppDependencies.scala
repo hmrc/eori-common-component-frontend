dec25d117a0f91a86b647c9e898cbbf8b8b21e82
lewis-nicholls <173899555+lewis-nicholls@users.noreply.github.com>
2026-07-14 11:11:46 +0100

    Upgrading dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,15 +4,15 @@ object AppDependencies {
   import play.core.PlayVersion
 
   val mongoDbVersion   = "2.12.0"
-  val bootstrapVersion = "10.7.0"
+  val bootstrapVersion = "10.8.0"
 
   val compileDependencies: Seq[ModuleID] = Seq(
     "org.typelevel"     %% "cats-core"                             % "2.13.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.5.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"            % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                    % mongoDbVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "13.4.0",
-    "uk.gov.hmrc"       %% "internal-auth-client-play-30"          % "4.3.0"
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "13.9.0",
+    "uk.gov.hmrc"       %% "internal-auth-client-play-30"          % "4.4.0"
   )
 
   val testDependencies: Seq[ModuleID] = Seq(
